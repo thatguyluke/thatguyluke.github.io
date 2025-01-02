1b595c4f881a73ff241d7cfdedd64f2ede21d2a8
ThatGuyLuke <45183678+thatguyluke@users.noreply.github.com>
2025-01-01 21:16:33 -0600

    getting ready to publish (but probably not)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://www.thatguyluke.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/thatguyluke.github.io',
+  baseUrl: '/thatguyluke.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'thatguyluke', // Usually your GitHub org/user name.
   projectName: 'thatguyluke.github.io', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
